d39de889359a1ccb419c7735d77af291ed14f20e
Elmer Gustavo <elmergustavo79@gmail.com>
2022-01-09 08:04:58 -0600

    Delete Curso de Postman/postman-course directory

--- Curso de Postman/postman-course/project/admin_info.sh
+++
@@ -1,4 +0,0 @@
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('superadmin', 'postmancourse@course.com', 'ThisIs@SecurePassw0rd')" | python manage.py shell
-
-
-python manage.py drf_create_token superadmin
--- Curso de Postman/postman-course/project/migrations/admin_migration.py
+++
@@ -1,6 +0,0 @@
-from courses.models import Course, Material, Teacher
-
-
-def run():
-    # This is where the faker must run
-    pass
